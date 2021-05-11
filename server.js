f9f6cd70f299903cdac0b57d32dda17043ad39e3
Joe Brimeyer <jbrimeyer@gmail.com>
2021-05-10 20:34:44 -0700

    set up passport strategy and added routes.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// passport = require("passport");
-// localStrategy = require("passport-local");
-// passportLocalMongoose = require("passport-local-mongoose");
+passport = require("passport");
+localStrategy = require("passport-local");
+passportLocalMongoose = require("passport-local-mongoose");
+
+var Strategy = require("passport-facebook").Strategy;
 
 const methodOverride = require("method-override");
 const session = require("express-session");
@@ -15,17 +17,54 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//middleware
+// Configure view engine to render EJS templates.
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
-//User session
-// app.use(
-//   session({
-//     secret: "oauth_facebook",
-//     resave: false,
-//     saveUninitialized: false,
-//   })
-// );
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require("morgan")("combined"));
+app.use(require("cookie-parser")());
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes.
+app.get("/", function (req, res) {
+  res.render("home", { user: req.user });
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/login/facebook", passport.authenticate("facebook"));
+
+app.get(
+  "/return",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+app.get(
+  "/profile",
+  require("connect-ensure-login").ensureLoggedIn(),
+  function (req, res) {
+    res.render("profile", { user: req.user });
+  }
+);
 
 app.use(methodOverride("_method"));
 
@@ -34,16 +73,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // FacebookStrategy within Passport
-// code goes here ......
-
-//homepage route
-// app.get("/", (req, res) => {
-//   res.send("OAuth-Facebook homepage");
-// });
-
-// routes
-// const userRouter = require("./routes/users.js");
-// app.use("/users", userRouter);
+passport.use(
+  new Strategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: "http://localhost:3000/auth/facebook/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 
 // Server connection.
 app.listen(PORT, () => {
